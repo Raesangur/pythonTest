81e1442f71bbac1af2db9e240bc0ec294d288f90
Raesangur <pascalpinguin@gmail.com>
2020-09-06 16:59:35 -0400

    playing with functions and their arguments

--- Hello World.py
+++ Hello World.py
@@ -37,3 +37,37 @@ var6 = str(var5)
 print(type(var5))
 print(*[var4, "is of type: ", FindType(var4)])
 print(*[var3[0], "is of type: ", FindType(var3[0])])
+
+print("No args: " + str(type(foo)))
+print("With args: " + str(type(myFunction)))
+
+def printNumber(num):
+    print(num)
+
+import inspect
+var3.append(printNumber)
+# Call a function with argument `12` only if the function takes arguments
+[x(12) for x in var3 if len(inspect.getfullargspec(x).args) != 0]
+
+
+def noArgs():
+    print("No arguments")
+
+def oneArgs(arg1):
+    print("One argument: " + str(arg1))
+
+def twoArgs(arg1, arg2):
+    print("Two arguments: " + str(arg1) + ", " + str(arg2))
+
+def threeArgs(arg1, arg2, arg3):
+    print("Three arguments: " + str(arg1) + ", " + str(arg2) + ", " + str(arg3))
+
+import random
+
+functionList = [noArgs * 2, oneArgs * 5, twoArgs * 3, threeArgs * 10]
+random.shuffle(functionList)
+
+print(functionList)
+
+# Generate a random integral argument value for each argument that a function takes in the list
+[x(*(random.sample(range(0, 65535), len(inspect.getfullargspec(x).args)))) for x in functionList]
